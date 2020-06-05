d7ee42c5bac04e9847c1db93cf8697d223134f11
Pedrolustosa <pedroeternalss@gmail.com>
2020-06-05 14:18:18 -0300

    Health1000 v1.9.1

--- prevencao.php
+++ prevencao.php
@@ -287,7 +287,7 @@
     <!--Script para o botão popover"Ajuda!"-->
 
     <!--Script's-->
-    <script src="js/form.js"></script>
+    <script type="text/javascript" src="js/form.js"></script>
     <script type="text/javascript" src="js/pace.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
